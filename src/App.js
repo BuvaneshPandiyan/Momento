366597041cce11036e473ab0f47ec002385a6bcb
BuvaneshPandiyan <buvaneshpandiyan@gmail.com>
2025-06-21 03:46:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar />
         <HomePage />
-    <Footer />
+        <Footer />
       </header>
     </div>
   );
